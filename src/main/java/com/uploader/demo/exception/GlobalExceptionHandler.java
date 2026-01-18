38d09fcc90ef2c19b1b367a25b7fd8b88f2b9054
KesarwaniHimanshu <himanshukesarwani16@gmail.com>
2026-01-18 20:22:00 +0530

    exception for generic error

--- src/main/java/com/uploader/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/uploader/demo/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.uploader.demo.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -48,17 +50,39 @@ public class GlobalExceptionHandler {
     }
 
     // ✅ All uncaught exceptions
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<ApiError> handleGeneric(Exception ex) {
+//
+//        ApiError apiError = new ApiError(
+//                LocalDateTime.now(),
+//                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+//                "Internal Server Error",
+//                ex.getMessage(),
+//                null
+//        );
+//
+//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
+//    }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiError> handleGeneric(Exception ex) {
+    public ResponseEntity<ProblemDetail> handleAll(Exception ex) {
 
-        ApiError apiError = new ApiError(
-                LocalDateTime.now(),
-                HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                "Internal Server Error",
-                ex.getMessage(),
-                null
-        );
+        ex.printStackTrace(); // 🔥 critical for debugging
 
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
+        ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        problem.setTitle("Internal Server Error");
+        problem.setDetail(ex.getMessage());
+        problem.setProperty("timestamp", LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(problem);
     }
+
+
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<ErrorResponse> handleAll(Exception ex) {
+//        ex.printStackTrace(); // VERY IMPORTANT in dev
+//        return ResponseEntity.status(500)
+//                .body(new ErrorResponse("Internal error", ex.getMessage()));
+//    }
 }
